d4fc251c4ba51ec1e09bf51fa80e867fdba61103
Manuel <manuel.t.shade@gmail.com>
2025-04-10 16:57:01 +0100

    Initial config

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: "about",
+    loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)
+  },
+  {
+    path: "recipes",
+    loadComponent: () => import('./recipes/recipes.component').then(m => m.RecipesComponent)
+  }
+];
