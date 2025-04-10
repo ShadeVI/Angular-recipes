5c46be0c0746ce60ed2d08650ef7170558d16c2c
Manuel <manuel.t.shade@gmail.com>
2025-04-10 18:58:52 +0100

    Add route data for improved SEO and user experience

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,34 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: "",
-    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
+    data: {
+      title: "Home - My Recipes App",
+      description: "Welcome to My Recipes App, where you can find and share delicious recipes!"
+    }
   },
   {
     path: "about",
-    loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)
+    loadComponent: () => import('./about/about.component').then(m => m.AboutComponent),
+    data: {
+      title: "About - My Recipes App",
+      description: "Learn more about My Recipes App and how to use it."
+    }
   },
   {
     path: "recipes",
-    loadComponent: () => import('./recipes/recipes.component').then(m => m.RecipesComponent)
+    loadComponent: () => import('./recipes/recipes.component').then(m => m.RecipesComponent),
+    data: {
+      title: "Recipes - My Recipes App",
+      description: "Browse through a collection of delicious recipes."
+    }
+  },
+  {
+    path: "recipes/:id",
+    loadComponent: () => import('./recipe-detail/recipe-detail.component').then(m => m.RecipeDetailComponent),
+    data: {
+      title: "Recipe Detail - My Recipes App",
+      description: "View the details of a specific recipe."
+    }
   }
 ];
