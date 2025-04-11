e4f30bd7afe7481dfb371757e17703887da5ce84
Manuel <manuel.t.shade@gmail.com>
2025-04-11 12:50:27 +0100

    Refactor RecipesService to use HttpClient for fetching recipes

--- src/app/recipes.service.ts
+++ src/app/recipes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, signal } from '@angular/core';
 import { Recipe } from './recipe';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +11,20 @@ export class RecipesService {
 
   recipesList = signal<Recipe[]>([])
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   async fetchAllRecipes() {
-    fetch(this.baseUrl + this.recipesUrl)
+    this.http.get<{ recipes: Recipe[], total: number, skip: number, limit: number }>(this.baseUrl + this.recipesUrl).subscribe((data) => {
+      this.recipesList.set(data.recipes)
+    })
+    /* fetch(this.baseUrl + this.recipesUrl)
       .then(response => response.json())
       .then(data => {
         this.recipesList.set(data.recipes);
       })
       .catch(error => {
         console.error('Error fetching recipes:', error);
-      }).finally(() => { console.log("Fetch done") });
+      }).finally(() => { console.log("Fetch done") }); */
   }
 }
