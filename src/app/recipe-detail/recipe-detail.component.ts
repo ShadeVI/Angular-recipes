2a7092e1594bdffadb44c3b8b02e5be91adce810
Manuel <manuel.t.shade@gmail.com>
2025-04-11 12:51:08 +0100

    Enhance RecipeDetailComponent to display recipe ID and utilize RouterLink for navigation

--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
 export class RecipeDetailComponent {
-
+  // Obtener el id de la receta desde la URL
+  route: ActivatedRoute = inject(ActivatedRoute)
+  recipeId: number = Number(this.route.snapshot.paramMap.get('id'))
 }
