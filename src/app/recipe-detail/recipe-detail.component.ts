040fb31a9dc4718e7cee0717469d67aaf6c75948
Manuel <manuel.t.shade@gmail.com>
2025-04-12 12:52:50 +0100

    Refactor RecipeDetailComponent to enhance recipe details display and improve data fetching

--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Recipe } from '../recipe';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,5 +12,15 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class RecipeDetailComponent {
   // Obtener el id de la receta desde la URL
   route: ActivatedRoute = inject(ActivatedRoute)
+  recipeService = inject(RecipesService)
   recipeId: number = Number(this.route.snapshot.paramMap.get('id'))
+  recipeDetails = signal<Recipe | null>(null)
+
+  constructor() {
+    effect(() => {
+      this.recipeService.fetchRecipeDetails(this.recipeId).subscribe((recipe) => {
+        this.recipeDetails.set(recipe)
+      })
+    })
+  }
 }
